8fe95374359ef00f23926be16a879ee1fa8415d5
worramet1163 <6206021621163@fitm.kmutnb.ac.th>
2021-03-18 11:14:16 +0700

    new

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 
-const DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://admin:1234@cluster0.co4mq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const DATABASE_URL =
+  process.env.DATABASE_URL ||
+  "mongodb+srv://admin:12345@cluster0.kvpij.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
 
-app.use(express.json())
+app.use(express.json());
 
-const subscribersRouter = require('./routes/subscribers')
-app.use('/subscribers', subscribersRouter)
+const subscribersRouter = require("./routes/subscribers");
+app.use("/subscribers", subscribersRouter);
 
-app.listen(3000, () => console.log('Server Started'))
+app.listen(3000, () => console.log("Server Started"));
